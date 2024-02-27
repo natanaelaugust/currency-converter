8087775578d1fd1718c201cc05fbc3b54b9e788f
Natanael Freitas <natanaelfreitas17@gmail.com>
2024-02-27 18:23:51 -0300

    Adicionando arquivos à nova branch

--- conversor.js
+++ conversor.js
@@ -1,19 +1,58 @@
 function converterMoeda() {
-  let valorEmReais = document.getElementById("valorEmReais").value;
-
-  if (isNaN(valorEmReais) || valorEmReais === "") {
-    alert("Insira um valor válido em Reais (R$)");
+  let primeiraMoeda = document.getElementById("primeiraMoeda").value;
+  let valorPrimeiraMoeda = parseFloat(document.getElementById("valorPrimeiraMoeda").value);
+  let taxaDeConversao = document.getElementById("segundaMoeda").value;
+  if (isNaN(valorPrimeiraMoeda) || valorPrimeiraMoeda <= 0) {
+    alert("Insira um valor válido e maior que zero.");
     return;
   }
-
-  var taxaDeConversao = 4.93;
-
-  var dolares = valorEmReais / taxaDeConversao;
-
-  document.getElementById("valorEmDolares").value = dolares.toFixed(2);
+  if (primeiraMoeda === taxaDeConversao) {
+    alert("Não é válido converter uma moeda para ela mesma ");
+    return;
+  }
+  switch (primeiraMoeda) {
+    case "dolarAmericano":
+      valorPrimeiraMoeda = valorPrimeiraMoeda * 4.95;
+      break;
+    case "libraEsterlina":
+      valorPrimeiraMoeda = valorPrimeiraMoeda * 6.25;
+      break;
+    case "euro":
+      valorPrimeiraMoeda = valorPrimeiraMoeda * 5.34;
+      break;
+    case "pesoArgentino":
+      valorPrimeiraMoeda = valorPrimeiraMoeda * 0.0060;
+      break;
+    case "real":
+      valorPrimeiraMoeda = valorPrimeiraMoeda * 1;
+      break;
+    default:
+      break;
+  }
+  switch (taxaDeConversao) {
+    case "dolarAmericano":
+      taxaDeConversao = 4.95;
+      break;
+    case "libraEsterlina":
+      taxaDeConversao = 6.25;
+      break;
+    case "euro":
+      taxaDeConversao = 5.34;
+      break;
+    case "pesoArgentino":
+      taxaDeConversao = 0.0060;
+      break;
+    case "real":
+      taxaDeConversao = 1;
+      break;
+    default:
+      break;
+  }
+  let valorFinal = valorPrimeiraMoeda / taxaDeConversao;
+  document.getElementById("valorFinal").value = valorFinal.toFixed(2);
 }
 
 function limparCampo() {
-  document.getElementById("valorEmReais").value = "";
-  document.getElementById("valorEmDolares").value = "";
+  document.getElementById("valorPrimeiraMoeda").value = "";
+  document.getElementById("valorFinal").value = "";
 }
